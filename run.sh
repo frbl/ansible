e4927171f12c70e686f6194bda87587402390a32
Frank Blaauw <frank.blaauw@gmail.com>
2019-09-15 12:27:02 +0200

    Added i3 status and zerorier

--- run.sh
+++ run.sh
@@ -2,19 +2,16 @@
 # Install ansible if not yet installed
 if [[ "$OSTYPE" == "darwin"* ]]; then
   # TODO: Install osx dependencies
+    ./bin/bootstrap_osx
   true
 else
   if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
-    sudo apt-get install -y software-properties-common
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-    sudo apt-add-repository ppa:ansible/ansible
-    sudo apt-get update
-    sudo apt-get install -y ansible
+    ./bin/bootstrap_debian
   fi
 fi
 
 #ansible workstations -m ping -i hosts --ask-pass --extra-vars "ansible_python_interpreter=/usr/bin/python3"
 #ansible-playbook --ask-pass -K -i hosts workstations.yml
 #ansible-playbook -K -i hosts workstations.yml
-ansible-playbook -K -i hosts site.yml -f 2
+ansible-playbook -K -k -i hosts site.yml -f 2
 #ansible-playbook -i hosts site.yml -f 2
