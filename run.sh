6916d83de40ba00d142c663f9f1c42df4a31185c
Frank Blaauw <frank.blaauw@gmail.com>
2020-03-07 21:01:12 +0100

    Updated docker

--- run.sh
+++ run.sh
@@ -7,11 +7,12 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 else
   if [ $(dpkg-query -W -f='${Status}' ansible 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
     ./bin/bootstrap_debian
+    ansible-galaxy install geerlingguy.docker
   fi
 fi
 
 #ansible workstations -m ping -i hosts --ask-pass --extra-vars "ansible_python_interpreter=/usr/bin/python3"
 #ansible-playbook --ask-pass -K -i hosts workstations.yml
-ansible-playbook -K -i hosts site.yml
+ansible-playbook -vvv -K -i hosts site.yml
 #ansible-playbook -K -k -i hosts site.yml -f 2
 #ansible-playbook -i hosts site.yml -f 2
